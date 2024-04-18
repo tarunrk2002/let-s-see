cf033f1feb88796fd7aa4d344d1417813e4f7754
tarun <tarunrk2002@gmail.com>
2024-04-18 11:46:57 +0530

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+
+
+        }
+    }
+}
