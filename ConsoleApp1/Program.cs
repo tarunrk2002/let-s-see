05d7948e040def67355d66c5f460609e81d4f1ab
tarun <tarunrk2002@gmail.com>
2024-05-02 13:00:47 +0530

    learnt accessing single element from list(object)

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,53 +8,89 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            List<object> list = new List<object>();
-            list.Add("ughr");
-            list.Add(3);
-            list.Add(4);
-            list.Add(5);
-            list.Add("eufeiuh");
+            //List<eco> ecos = new List<eco>();
+            //ecos.Add(new eco
+            //{
+            //    name = "Tededst",
+            //    description = "Teset1",
+            //    type = "dewef"
+            //});
 
-           
+            //ecos.Add(new eco
+            //{
+            //    name = "Teerfst",
+            //    description = "Teserft1",
+            //    type = "deef"
+            //});
 
-            List<eco> result = new List<eco>();
-            result.Add(new eco
+            //ecos.Add(new eco
+            //{
+            //    name = "Testfr",
+            //    description = "Test1efw",
+            //    type = "dewef"
+            //});
+
+            
+            //foreach (eco e in ecos)
+            //{
+            //    Console.WriteLine(e.name);
+            //    Console.WriteLine(e.description);
+            //    Console.WriteLine(e.type);
+            //    Console.WriteLine(e);
+            //}
+            //Console.WriteLine();
+
+            //for (int i = 0; i < ecos.Count; i++)
+            //{
+            //    Console.WriteLine();
+            //}
+
+
+            List<int> ints = new List<int>();
+            ints.Add(032);
+            ints.Add(123);
+            ints.Add(24);
+            ints.Add(344444443);
+
+            Console.WriteLine(ints[1]);
+
+            List<eco> ecoob = new List<eco>();
+            ecoob.Add(new eco
             {
-                name = "fgrg",
-                description = "ererf",
-                type = "dont'ert"
+                name = "a",
+                description = "b",
+                type = "c"
             });
-            result.Add(new eco
+            ecoob.Add(new eco
             {
-                name = "fgrg",
-                description = "ererf",
-                type = "dont'ert"
-            });
-            result.Add(new eco
-            {
-                name = "fgrg",
-                description = "ererf",
-                type = "dont'ert"
+                name = "ade",
+                description = "wadb",
+                type = "caed"
             });
 
-            Console.WriteLine(result);
-            foreach (var item in result)
+            ecoob.Add(new eco
             {
-                Console.WriteLine(item.name);
-                Console.WriteLine(item.description);
-                    Console.WriteLine(item.type);
-                Console.WriteLine("$$$$$$$$$$$$$$$$$$$$$$$$$$$$");
-            }
-            var resultq = new List<eco>();
-
-            resultq.Add(new eco
-            {
-                name = "hu4hr"
-
+                name = "afef",
+                description = "bef",
+                type = "sdc"
             });
 
-            Console.WriteLine(resultq.Count);
-            Console.WriteLine(resultq[0]);
+
+
+
+            string name;
+            name = ecoob[2].name;
+            Console.WriteLine(name);
+            Console.WriteLine(ecoob[1].name);
+
+
+
+
+
+
+
+
+
         }
        
     }
